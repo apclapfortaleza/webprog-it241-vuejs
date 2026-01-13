396df78c37cbc28ec35827d98915c97125cbdf8c
Lance Art P Fortaleza <lpfortaleza@student.apc.edu.ph>
2026-01-13 02:33:26 +0000

    .

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Apples and Bananas',
+      message: 'I like apples and bananas'
+    };
+  }
+};
+</script>
+
+<style>
+</style>
